19523e61fc7635883d1855363e1f9bda7bc32860
Rashidello <rashidello.rayanov@gmail.com>
2024-10-20 17:05:59 +0400

    all bugs fixed ig

--- Venting machine.py
+++ Venting machine.py
@@ -49,38 +49,39 @@ def dic():
 	}
 	start()
 
+
 def userinput():
 	global userchoice
-	userchoice = int(input("choose an item using their code "))
-	#checks if item in the vending machine is present
+	userchoice = int(input("Choose an item using their code "))
 
+	#checks if item in the vending machine is present
 	if userchoice in item:
 		#checks stock of userchoice
 		if item[userchoice]["Stock"] == 0:
-			print("sorry we're out of chips, please try again\n\n")
+			print("sorry, we're out of chips, please try again\n")
 			userinput()
 			#343 doesn't have any additional snacks 
 		elif userchoice == 343:
-			print(f"you have choose {item[userchoice]['Item'.capitalize()]}, it will cost {item[userchoice]['Price'] }AED")
+			print(f"You have chosen {item[userchoice]['Item']}, it will cost {item[userchoice]['Price'] } AED")
 			payment()		
 		else:
-			print(f"you have choose {item[userchoice]['Item'.capitalize()]}, it will cost {item[userchoice]['Price'] }AED")
+			print(f"You have chosen {item[userchoice]['Item']}, it will cost {item[userchoice]['Price'] } AED")
 			recommendations()
 	else:
-		print("it's not there:<")
+		print("It's not there:<")
 		start()
 
 
+
 def recommendations():
-	global additem
 	#classic
 	if userchoice == 933:
-		additem = input("would you like to have some Mentos?(y/n): ")
+		additem = input("Would you like to have some Mentos?(y/n): ")
 		if additem == 'y':
 			additem = 311
 			#cancels additional item if it's stock equals to 0
 			if item[additem]["Stock"] == 0:
-				print(f"sorry we're out of {item[additem]["Item"]}")
+				print(f"Sorry we're out of {item[additem]['Item']}")
 				additem = 0
 			payment()
 		else:
@@ -88,33 +89,33 @@ def recommendations():
 			payment()
 	#classic v2
 	elif userchoice == 311:
-		additem = input("would you like to have some Soda?(y/n): ")
+		additem = input("Would you like to have some Soda?(y/n): ")
 		if additem == 'y':
 			additem = 933
 			if item[additem]["Stock"] == 0:
-				print(f"sorry we're out of {item[additem]["Item"]}")
+				print(f"Sorry we're out of {item[additem]['Item']}")
 				additem = 0
 			payment()
 		else:
 			additem = 0
 			payment()
 	elif userchoice == 209:
-		additem = input("would you like to have some Cookie?(y/n): ")
+		additem = input("Would you like to have some Cookie?(y/n): ")
 		if additem == 'y':
 			additem = 878
 			if item[additem]["Stock"] == 0:
-				print(f"sorry we're out of {item[additem]["Item"]}")
+				print(f"Sorry we're out of {item[additem]['Item']}")
 				additem = 0
 			payment()
 		else:
 			additem = 0
 			payment()
 	elif userchoice == 878:
-		additem = input("would you like to have some Milk?(y/n): ")
+		additem = input("Would you like to have some Milk?(y/n): ")
 		if additem == 'y':
 			additem = 209
 			if item[additem]["Stock"] == 0:
-				print(f"sorry we're out of {item[additem]["Item"]}")
+				print(f"Sorry we're out of {item[additem]['Item']}")
 				additem = 0
 			payment()
 		else:
@@ -124,6 +125,7 @@ def recommendations():
 	else:
 		#2:52am
 		print("You broke the system!")
+
 
 #fucntion where payment is going
 def payment():
@@ -139,10 +141,14 @@ def payment():
 	userwallet = int(input(f"pay {totalamount}AED "))
 	#checks if user gave correct amount of money
 	if userwallet < totalamount:
-		print("print required ammount")
+		print("Pay the required amount")
 		payment()
 	userwallet = userwallet - totalamount
-	print(f"you wallet is {userwallet}")
+	#shows items bought by user and user's change
+	if additem > 0:
+		print(f"You got {item[userchoice]['Item']} and {item[additem]['Item']}\nYour change is {userwallet}")
+	else:
+		print(f"You got {item[userchoice]['Item']}\nYour change is {userwallet}")
 
 	#Stock substraction
 	item[userchoice]["Stock"] -= 1
@@ -151,6 +157,9 @@ def payment():
 	#repeat if user wants
 	repeat = input("Wanna buy something again?(y/n) ")
 	if repeat == 'y':
+		#updating value in variable "additem" because additional item would be saved even if u have chosen one item
+		additem = 0
+
 		start()
 	else:
 		exit()
@@ -159,13 +168,14 @@ def payment():
 
 #start of an code
 def start():
-	print("Welcome hren")
+	print("Welcome! here's some stuff that we have:>\n")
 	#prints items using for loop
 	for keys,values in item.items():
 		print(f"Code: {values['Code']}, Item: {values['Item']}, Category: {values['Category']}, Price: {values['Price']}, Stock: {values['Stock']}")
 	#users options with numbers
 	fchoice = int(input("\nUser's options:\n1.Cancel\n2.Choose category:snack\n3.Choose category:drinks\n4.Choose an item\n(1-4)\n"))
 	if fchoice == 1:
+		print("Bye bye!")
 		exit()
 	#option 2 & 3 are using for loop to show spesicic category and have function for user's input
 	elif fchoice == 2:
@@ -199,7 +209,7 @@ def start():
 		userinput()
 	#restarts program incase user writes any other nomber except 1-4
 	else:
-		print("worng!\nTry again!!")
+		print("Wrong!\nTry again!!")
 		start()
 
 dic()
